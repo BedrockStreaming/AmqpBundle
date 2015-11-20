bccff912f3a0b02259cc63e4e4793dae8228ddc7
Fabien DE SAINT PERN <fdesaintpern@m6web.fr>
2015-11-20 18:01:03 +0100

    Fix Notice when exchange type not provided

--- src/AmqpBundle/Factory/AMQPFactory.php
+++ src/AmqpBundle/Factory/AMQPFactory.php
@@ -21,14 +21,18 @@ abstract class AMQPFactory
         /** @var \AMQPExchange $exchange */
         $exchange = new $exchangeClass($channel);
         $exchange->setName($exchangeOptions['name']);
-        $exchange->setType($exchangeOptions['type']);
-        $exchange->setArguments($exchangeOptions['arguments']);
-        $exchange->setFlags(
-            ($exchangeOptions['passive'] ? AMQP_PASSIVE : AMQP_NOPARAM) |
-            ($exchangeOptions['durable'] ? AMQP_DURABLE : AMQP_NOPARAM) |
-            ($exchangeOptions['auto_delete'] ? AMQP_AUTODELETE : AMQP_NOPARAM)
-        );
-        $exchange->declareExchange();
+
+        // If the type is not specified, the exchange must exist
+        if (isset($exchangeOptions['type'])) {
+            $exchange->setType($exchangeOptions['type']);
+            $exchange->setArguments($exchangeOptions['arguments']);
+            $exchange->setFlags(
+                ($exchangeOptions['passive'] ? AMQP_PASSIVE : AMQP_NOPARAM) |
+                ($exchangeOptions['durable'] ? AMQP_DURABLE : AMQP_NOPARAM) |
+                ($exchangeOptions['auto_delete'] ? AMQP_AUTODELETE : AMQP_NOPARAM)
+            );
+            $exchange->declareExchange();
+        }
 
         return $exchange;
     }
